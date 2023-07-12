baa313ffc0f7897cb1757fa6cef8c016c274fe9c
Mohammadaman <majmeri@flemingcollege.ca>
2023-07-12 15:44:53 -0400

    Script 1 completed

--- create_db.py
+++ create_db.py
@@ -7,26 +7,59 @@ Usage:
  python create_db.py
 """
 import os
+import sqlite3
+from datetime import datetime
 
 # Determine the path of the database
 script_dir = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(script_dir, 'social_network.db')
 
+con = sqlite3.connect('social_network.db')
+cur = con.cursor()
 def main():
     create_people_table()
     populate_people_table()
 
 def create_people_table():
-    """Creates the people table in the database"""
-    # TODO: Create function body
-    # Hint: See example code in lab instructions entitled "Creating a Table"
-    return
+# Commit (save) pending transactions to the database.
+# Transactions must be committed to be persistent.
+ return
 
 def populate_people_table():
     """Populates the people table with 200 fake people"""
     # TODO: Create function body
+    add_person_query = """
+    INSERT INTO people
+    (
+    name,
+    email,
+    address,
+    city,
+    province,
+    bio,
+    age,
+    created_at,
+    updated_at
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+"""
     # Hint: See example code in lab instructions entitled "Inserting Data into a Table"
+    new_person = ('Beast',
+                  'beastyboy07@gmail.com',
+                  '111 fake add st.',
+                  'FakerSpot',
+                  25,
+                  datetime.now(),
+                  datetime.now())
+    cur.execute(add_person_query, new_person)
+    con.commit()
+    con.close()
     # Hint: See example code in lab instructions entitled "Working with Faker"
+    fake = ("en_CA")
+    for _ in range(10):
+       province = fake.administrative_unit()
+       population = fake.random_int(min=900000, max=100000000)
+       print(f'The population of {province} is {population}.')
     return
 
 if __name__ == '__main__':
